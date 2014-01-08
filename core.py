c367ca4fb102e432cbb8da5d29828c37b272862c
RamtinA <ramtin.number1@gmail.com>
2014-01-08 20:02:50 +0330

    Start again

--- core.py
+++ core.py
@@ -1,17 +1,3 @@
 from gi.repository import Gtk
 
-class Handler:
-    def onDeleteWindow(self, *args):
-        Gtk.main_quit(*args)
-
-    def onButtonPressed(self, button):
-        print("Hello World!")
-
-builder = Gtk.Builder()
-builder.add_from_file("gui.glade")
-builder.connect_signals(Handler())
-
-window = builder.get_object("window1")
-window.show_all()
-
-Gtk.main()
+# We will start from here
