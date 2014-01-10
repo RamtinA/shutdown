080f849f09edd0ab75a549ddaec58a36c037ebc2
RamtinA <ramtin.number1@gmail.com>
2014-01-11 00:02:39 +0330

    GUI is almost done

--- core.py
+++ core.py
@@ -17,50 +17,51 @@ class AppWindow(Gtk.Window):
 		BBox = Gtk.Box(spacing = 10)
 
 		#Label
-		StatusLabel = Gtk.Label("Select One of the options")
+		self.StatusLabel = Gtk.Label("Select One of the options")
 
 		#SpinButtons
 		HAdjustment = Gtk.Adjustment(0, 0, 23, 1, 10, 0)
 		MAdjustment = Gtk.Adjustment(0, 0, 59, 1, 10, 0)	
 
-		SBHours = Gtk.SpinButton()
-		SBHours.set_adjustment(HAdjustment)
+		self.SBHours = Gtk.SpinButton()
+		self.SBHours.set_adjustment(HAdjustment)
 
-		SBMinutes = Gtk.SpinButton()
-		SBMinutes.set_adjustment(MAdjustment)
+		self.SBMinutes = Gtk.SpinButton()
+		self.SBMinutes.set_adjustment(MAdjustment)
 
 		#RadioButtons
-		ShutdownRB = Gtk.RadioButton.new_with_label_from_widget(None,"Shutdown")
-		ShutdownRB.connect("toggled",self.on_button_toggled, "shutdown")
+		self.ShutdownRB = Gtk.RadioButton.new_with_label_from_widget(None,"Shutdown")
+		self.ShutdownRB.connect("toggled",self.on_button_toggled, "shutdown")
 
-		RestartRB = Gtk.RadioButton.new_from_widget(ShutdownRB)
-		RestartRB.set_label("Restart")
-		RestartRB.connect("toggled",self.on_button_toggled, "restart")
+		self.RestartRB = Gtk.RadioButton.new_from_widget(self.ShutdownRB)
+		self.RestartRB.set_label("Restart")
+		self.RestartRB.connect("toggled",self.on_button_toggled, "restart")
 
-		HaltRB = Gtk.RadioButton.new_from_widget(ShutdownRB)
-		HaltRB.set_label("Halt")
-		HaltRB.connect("toggled",self.on_button_toggled, "halt")
+		self.HaltRB = Gtk.RadioButton.new_from_widget(self.ShutdownRB)
+		self.HaltRB.set_label("Halt")
+		self.HaltRB.connect("toggled",self.on_button_toggled, "halt")
 
 		#Buttons
-		DoneButton = Gtk.Button("Done")
-		DoneButton.connect("clicked",self.Done_Clicked)
+		self.DoneButton = Gtk.Button("Done")
+		self.DoneButton.connect("clicked",self.Done_Clicked)
 
-		CancelButton =Gtk.Button("Cancel")
+		self.CancelButton = Gtk.Button("Cancel")
+		self.CancelButton.connect("clicked",self.Cancel_Clicked)
 
 		#Boxes
 		##Horizontal Boxes
-		SpinBox.pack_start(SBHours,True,True,0)
-		SpinBox.pack_start(SBMinutes,True,True,0)
+		SpinBox.pack_start(self.SBHours,True,True,0)
+		SpinBox.pack_start(self.SBMinutes,True,True,0)
 
-		RBBox.pack_start(ShutdownRB,True,True,0)
-		RBBox.pack_start(RestartRB,True,True,0)
-		RBBox.pack_start(HaltRB,True,True,0)
+		RBBox.pack_start(self.ShutdownRB,True,True,0)
+		RBBox.pack_start(self.RestartRB,True,True,0)
+		RBBox.pack_start(self.HaltRB,True,True,0)
 
-		BBox.pack_start(DoneButton,True,True,0)
-		BBox.pack_start(CancelButton,True,True,0)
+		BBox.pack_start(self.DoneButton,True,True,0)
+		BBox.pack_start(self.CancelButton,True,True,0)
 
 		#Vertical Boxes
-		vbox.pack_start(StatusLabel,False,False,2)
+		vbox.pack_start(self.StatusLabel,False,False,2)
 		vbox.pack_start(SpinBox,False,False,2)
 		vbox.pack_start(RBBox,False,False,2)
 		vbox.pack_start(BBox,False,False,2)
@@ -73,16 +74,29 @@ class AppWindow(Gtk.Window):
 		if button.get_active():
 			state = "on"
 			labeltext = "You want",name,"your pc at"
-			self.StatusLabel.set_text("test")
+			self.StatusLabel.set_text("You Want "+name +" at")
 		else:
 			state = "off"
-		print("Button", name, "was turned", state)
+
 
 
 	def Done_Clicked(self,button):
 		print "Done Clicked"
+		self.SBHours.set_sensitive(False)
+		self.SBMinutes.set_sensitive(False)
 		self.DoneButton.set_sensitive(False)
+		self.ShutdownRB.set_sensitive(False)
+		self.RestartRB.set_sensitive(False)
+		self.HaltRB.set_sensitive(False)
 
+	def Cancel_Clicked(self,button):
+		print "Done Clicked"
+		self.SBHours.set_sensitive(True)
+		self.SBMinutes.set_sensitive(True)
+		self.DoneButton.set_sensitive(True)
+		self.ShutdownRB.set_sensitive(True)
+		self.RestartRB.set_sensitive(True)
+		self.HaltRB.set_sensitive(True)
 
 
 win = AppWindow()
